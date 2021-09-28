52c27bd36bbfa22e2c32d668b1c7000a637d0abc
Zain Uppal <zainuppal1@gmail.com>
2021-09-28 15:56:54 +0100

    add basic template

---
+++ server/data.js
@@ -0,0 +1,20 @@
+let queries = [
+    {id:1, cat :'cat', title:'nameOfWebsite', url:'url'},
+    {id:2, cat :'meme', title:'nameOfWebsite', url:'url'},
+    {id:3, cat :'cat', title:'nameOfWebsite', url:'url'},
+    {id:4, cat :'cat', title:'nameOfWebsite', url:'url'},
+    {id:5, cat :'cat', title:'nameOfWebsite', url:'url'},
+    {id:6, cat :'cat', title:'nameOfWebsite', url:'url'},
+    {id:7, cat :'cat', title:'nameOfWebsite', url:'url'},
+    {id:8, cat :'cat', title:'nameOfWebsite', url:'url'},
+    {id:9, cat :'cat', title:'nameOfWebsite', url:'url'},
+    {id:10, cat :'cat', title:'nameOfWebsite', url:'url'},
+    {id:11, cat :'cat', title:'nameOfWebsite', url:'url'},
+    {id:12, cat :'cat', title:'nameOfWebsite', url:'url'},
+    {id:13, cat :'cat', title:'apples are good', url:'url'},
+    {id:14, cat :'cat', title:'nameOfWebsite', url:'url'},
+    {id:15, cat :'cat', title:'nameOfWebsite', url:'url'},
+    {id:16, cat :'cat', title:'nameOfWebsite', url:'url'}
+]
+
+module.exports = queries
