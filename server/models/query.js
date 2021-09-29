9ce8a42643b347478459db2b25ec3d3c0e84cf94
Forum Desai <forumd1999@gmail.com>
2021-09-29 10:02:52 +0100

    Add description to class declaration

--- server/models/query.js
+++ server/models/query.js
@@ -1,16 +1,18 @@
-const queryData = require('../data')
+const queryData = require('../data');
 
 class Query {
     constructor(data){
         this.id = data.id;
-        this.cat = data.cat;
-        this.title = data.title
-        this.url = data.url
+        this.category = data.category;
+        this.title = data.title;
+        this.url = data.url;
+        this.description = data.description;
     }
+
     static get all(){
         const queries = queryData.map((q) => new Query(q));
         return queries;
     }
 }
 
-module.exports = Query
+module.exports = Query;
